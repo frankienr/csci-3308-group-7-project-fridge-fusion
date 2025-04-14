e5103b7abcef99a4d17eb36ddd8952dd59a9e011
Hanna Choi <choihan006@gmail.com>
2025-04-14 00:05:58 -0600

    added ability to select friends on fuse page

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -768,30 +768,37 @@ app.post('/friends/delete', async (req, res) => {
   }
 });
 
-app.get("/fuse", (req, res) => {
+app.get("/fuse", async (req, res) => {
   if(!req.session.user){
     return res.redirect('/login');
   }
   
-  res.render("pages/fuse",{friends:[{username:'Hanna'},{username:'Frankie'},{username:'Rodrigo'},{username:'Robert'},{username:'Hanna1'},{username:'Frankie1'},{username:'Rodrigo1'},{username:'Robert1'}, {username:'Hanna2'},{username:'Frankie2'},{username:'Rodrigo2'},{username:'Robert2'}], recipes: [
-    {recipe_id: 1,
-    title: 'Beef Pot Pies with Irish Cheddar Crust',
-    readyInMinutes: 45,
-    sourceURL: 'https://www.foodista.com/recipe/6FMXSM3N/beef-pot-pies-with-irish-cheddar-crust',
-    image: 'https://img.spoonacular.com/recipes/634660-312x231.jpg',
-    matchcount: '4'
-    },
-    {
-    recipe_id: 4,
-    title: 'Beef Lo Mein Noodles',
-    readyInMinutes: 45,
-    sourceURL: 'https://www.foodista.com/recipe/8QKG6BC8/beef-lo-mein',
-    image: 'https://img.spoonacular.com/recipes/634629-312x231.jpg',
-    matchcount: '1'
-    }
-    ]
-  })
-})
+  try {
+    // get the friends of the user
+    const friendsQuery = `
+      SELECT u.user_id, u.first_name, u.last_name, u.username 
+      FROM users u
+      JOIN user_friends uf ON u.user_id = uf.friend_id
+      WHERE uf.user_id = $1
+      ORDER BY u.first_name, u.last_name`;
+    
+    const friends = await db.any(friendsQuery, [req.session.user]);
+    
+    res.render("pages/fuse", {
+      friends: friends,
+      recipes: []
+    });
+    
+  } catch (error) {
+    console.error("Error loading fuse page:", error);
+    res.render("pages/fuse", {
+      error: true,
+      message: "Failed to load fusion page",
+      friends: [],
+      recipes: []
+    });
+  }
+});
 
 app.get('/logout', (req, res) => {
   req.session.destroy(function(err) {
