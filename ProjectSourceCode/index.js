0a5d2fb94c8e446aeec7328c001b69c97e9796fa
Robert Schmidt <hoppyrabbit27@gmail.com>
2025-04-16 13:52:02 -0600

    Changed index.js file to acces enviornment variblaes

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -22,8 +22,8 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-    host: 'db',
-    port: 5432,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
